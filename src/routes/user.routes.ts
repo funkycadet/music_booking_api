e17ee894d7becd9bd9e6dc5cb72fd2566891d187
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-04-02 10:46:07 +0100

    update: setup access control on certain routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,17 @@
 import { UserController } from '../controllers';
 import { Router } from 'express';
-import { validateReqBody, validateJWT } from '../middlewares';
+import { validateJWT, checkRole } from '../middlewares';
 
 const userController = new UserController();
 const userRouter = Router();
 
-userRouter.get('/', userController.getAll);
-userRouter.get('/me', validateJWT(), userController.getMe);
-userRouter.get('/:id', userController.getUserById);
-userRouter.get('/user/:params', userController.getUser);
+userRouter.use(validateJWT());
+
+userRouter.get('/', checkRole('admin'), userController.getAll);
+userRouter.get('/me', userController.getMe);
+userRouter.get('/:id', checkRole('admin'), userController.getUserById);
+// userRouter.get('/user/:params', userController.getUser);
 userRouter.patch('/:id', userController.updateUser);
+userRouter.delete('/:id', checkRole('admin'), userController.deleteUser);
 
 export default userRouter;
