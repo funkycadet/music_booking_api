21a8ce8ec2a11f92d4e41125b69acb5caa24a839
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-04-02 10:41:22 +0100

    update: refactor service methods and add method to update and create artist profile

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,26 +1,55 @@
 import { db } from '../database';
-import { IUser, IUserSignup } from '../interfaces';
+import { NotFoundError } from '../exceptions';
+import { IUser, IUserSignup, Pageable, Page } from '../interfaces';
+import { pageableHandler } from '../utils';
+import { Prisma } from '@prisma/client';
 
 class UserService {
-  public async getAllUsers(skip: number, limit: number): Promise<IUser[]> {
-    return await db.user.findMany({
-      skip,
-      take: limit,
-    });
+  public async getAllUsers(query: Pageable<Prisma.UserWhereInput>): Promise<Page<IUser>> {
+    const pageableQuery = pageableHandler.process<Prisma.UserWhereInput>(query);
+
+    const [count, data] = await Promise.all([
+      db.user.count({ where: pageableQuery.filter }),
+      db.user.findMany({
+        skip: pageableQuery.skip,
+        take: pageableQuery.limit,
+        where: pageableQuery.filter,
+        // include: {
+        //   profile: true,
+        // },
+        orderBy: pageableQuery.orderBy || { createdAt: 'desc' },
+      }),
+    ]);
+
+    return pageableHandler.responseToPageable(pageableQuery, count, data);
   }
 
   public async getUserById(id: string): Promise<IUser> {
-    return await db.user.findUnique({
+    const user = await db.user.findUnique({
       where: {
         id,
       },
     });
+
+    if (!user) {
+      throw new NotFoundError('User not found!');
+    }
+
+    return user;
   }
 
-  public async getUser(filter: object): Promise<IUser> {
-    return await db.user.findFirst({
-      where: filter,
+  public async getUser(filter: Partial<IUser>): Promise<IUser> {
+    const user = await db.user.findFirst({
+      where: {
+        ...filter,
+      },
     });
+
+    if (!user) {
+      throw new NotFoundError('User not found!');
+    }
+
+    return user;
   }
 
   public async createUser(data: IUserSignup): Promise<IUser> {
@@ -42,6 +71,52 @@ class UserService {
     return await db.user.delete({
       where: {
         id,
+      },
+    });
+  }
+
+  public async addProfile(id: string, data: object): Promise<IUser> {
+    const user = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundError('User not found!');
+    }
+
+    return await db.user.update({
+      where: {
+        id,
+      },
+      data: {
+        profile: {
+          create: data,
+        },
+      },
+    });
+  }
+
+  public async updateProfile(id: string, data: object): Promise<IUser> {
+    const user = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundError('User not found!');
+    }
+
+    return await db.user.update({
+      where: {
+        id,
+      },
+      data: {
+        profile: {
+          update: data,
+        },
       },
     });
   }
