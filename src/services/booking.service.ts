e919396245573f02b12f330e70dc8b0441259017
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-04-04 04:21:59 +0100

    update: add method to get all bookings

--- src/services/booking.service.ts
+++ src/services/booking.service.ts
@@ -5,35 +5,35 @@ import { pageableHandler } from "../utils";
 import { Prisma } from "@prisma/client";
 
 export default class BookingService {
-  // public async getAllBookings(query: Pageable<Prisma.BookingWhereInput>): Promise<Page<IBooking>> {
-  //   const pageableQuery = pageableHandler.process<Prisma.BookingWhereInput>(query);
+  public async getAllBookings(query: Pageable<Prisma.BookingWhereInput>): Promise<Page<IBooking>> {
+    const pageableQuery = pageableHandler.process<Prisma.BookingWhereInput>(query);
 
-  //   const [count, data] = await Promise.all([
-  //     db.booking.count({ where: pageableQuery.filter }),
-  //     db.booking.findMany({
-  //       skip: pageableQuery.skip,
-  //       take: pageableQuery.limit,
-  //       where: pageableQuery.filter,
-  //       orderBy: pageableQuery.orderBy || { createdAt: 'desc' },
-  //     }),
-  //   ]);
+    const [count, data] = await Promise.all([
+      db.booking.count({ where: pageableQuery.filter }),
+      db.booking.findMany({
+        skip: pageableQuery.skip,
+        take: pageableQuery.limit,
+        where: pageableQuery.filter,
+        orderBy: pageableQuery.orderBy || { createdAt: 'desc' },
+      }),
+    ]);
 
-  //   return pageableHandler.responseToPageable(pageableQuery, count, data);
-  // }
+    return pageableHandler.responseToPageable(pageableQuery, count, data);
+  }
 
-  // public async getBooking(filter: Partial<IBooking>): Promise<IBooking> {
-  //   const booking = await db.booking.findFirst({
-  //     where: {
-  //       ...filter,
-  //     },
-  //   });
+  public async getBooking(filter: Partial<Prisma.BookingWhereInput>): Promise<IBooking> {
+    const booking = await db.booking.findFirst({
+      where: {
+        ...filter,
+      },
+    });
 
-  //   if (!booking) {
-  //     throw new NotFoundError("Booking not found!");
-  //   }
+    if (!booking) {
+      throw new NotFoundError("Booking not found!");
+    }
 
-  //   return booking;
-  // }
+    return booking;
+  }
 
   public async getBookingById(id: string): Promise<IBooking> {
     const booking = await db.booking.findUnique({
