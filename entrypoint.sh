9543dafa964b675e42b0c1897754ccf709e9a1d3
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-04-04 05:03:27 +0100

    fix: remove database check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Waiting for database..."
 
-while ! nc -z db 5432; do
-  sleep 1
-done
+# while ! nc -z db 5432; do
+#   sleep 1
+# done
 
 echo "Database started"
 
@@ -13,4 +13,6 @@ yarn migration:generate
 
 yarn prisma:seed
 
-yarn build && yarn start
+yarn build
+
+yarn start
